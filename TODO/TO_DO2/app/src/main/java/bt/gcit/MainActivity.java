e1399a0c045cfa0f35366930358a74aecf3f6b1b
kinzang20 <12190062.gcit@rub.edu.bt>
2021-03-26 22:23:00 +0600

    FIRST

---
+++ TODO/TO_DO2/app/src/main/java/bt/gcit/MainActivity.java
@@ -0,0 +1,14 @@
+ package bt.gcit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
