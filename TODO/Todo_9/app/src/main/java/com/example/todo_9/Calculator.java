7dbd4f592a7c8c31ad0550f37551505b3636a297
kinzang20 <12190062.gcit@rub.edu.bt>
2021-04-27 12:43:40 +0600

    second

---
+++ TODO/Todo_9/app/src/main/java/com/example/todo_9/Calculator.java
@@ -0,0 +1,16 @@
+package com.example.todo_9;
+
+public class Calculator {
+    public static double add(double value1, double value2){
+        return value1 + value2;
+    }
+    public static double sub(double value1, double value2){
+        return value1 - value2;
+    }
+    public static double mul(double value1, double value2){
+        return value1 * value2;
+    }
+    public static double div(double value1, double value2){
+        return value1 / value2;
+    }
+}
